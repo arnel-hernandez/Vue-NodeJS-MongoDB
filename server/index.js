8723d4ab99cd312ee729d04223490e6cc8a28e65
samuel <aarnelhernandez@gmail.com>
2019-05-30 10:04:48 -0500

    Added Static Folder & prep spa

--- server/index.js
+++ server/index.js
@@ -1,14 +1,25 @@
 
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser')
 
 const app = express();
 
 //Middleware
+app.use(bodyParser.json())
 app.use(cors());
 
 const posts = require('./routes/api/post')
 app.use('/api/posts', posts)
+
+//Handle Production
+if(process.env.NODE_ENV === 'production'){
+    //Static folder
+    app.use(express.static(__dirname + '/public'));
+
+    //Handle SPA
+    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
 
 const port = process.env.PORT || 5000;
 
